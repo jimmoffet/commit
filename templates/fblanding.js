8457fa66508a41f9a0eae148afb87939b054b096
jimmoffet <jimmoffet@gmail.com>
2017-10-28 23:05:21 -0400

    write token to db

--- templates/fblanding.js
+++ templates/fblanding.js
@@ -78,6 +78,7 @@ window.fbAsyncInit = function() {
         $.ajax({
             url: '/api',
             data: postDict,
+            dataType: 'json',
             type: 'POST',
             success: function(response) {
                 console.log(response);
@@ -124,22 +125,4 @@ window.fbAsyncInit = function() {
       console.log(request.statusText);
       console.log(request.response);
     }
-
-  $(function() {
-    $('button').click(function() {
-        var user = $('#txtUsername').val();
-        var pass = $('#txtPassword').val();
-        $.ajax({
-            url: '/signUpUser',
-            data: $('form').serialize(),
-            type: 'POST',
-            success: function(response) {
-                console.log(response);
-            },
-            error: function(error) {
-                console.log(error);
-            }
-        });
-    });
-  });
 
