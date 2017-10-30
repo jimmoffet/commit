54a50ffe177f458b4be2f538f8f74dbe176b4043
jimmoffet <jimmoffet@gmail.com>
2017-10-30 18:55:29 -0400

    local fixes

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def apiTest():
 
         uid = json_dict['uid']
         longToken = json_dict['longToken']
-        friends = json_dict['taggable_friends']
+        friends = ''
         timeStamp = datetime.datetime.now()
         message1 = ""
         message2 = ""
