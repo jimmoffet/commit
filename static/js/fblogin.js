751f5f65114598251d30241f033d925ef314c4b6
jimmoffet <jimmoffet@gmail.com>
2017-11-02 00:28:59 -0400

    style login page

--- static/js/fblogin.js
+++ static/js/fblogin.js
@@ -52,6 +52,7 @@ window.fbAsyncInit = function() {
     } else {
       // The person is not logged into your app or we are unable to tell.
       //document.getElementById('status').innerHTML = 'Please log ' + 'into this app.';
+      window.location = "../../fancycommitlanding";
     }
   }
 
