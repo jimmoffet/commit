7a4fb07b5c3a607e464e00dadeb48e615d370581
jimmoffet <jimmoffet@gmail.com>
2017-11-04 14:27:18 -0400

    prep for db

--- app.py
+++ app.py
@@ -25,9 +25,9 @@ db = SQLAlchemy(app)
 
 from models import User
 
-user = User('jared', datetime.datetime.now(), 'jrjohns@mit.edu', '3280803892', '508508393', '83082982983', '930290903', '3989809808', 'hi', 'oh no', datetime.datetime.now(), '5000')
-db.session.add(user)
-db.session.commit()
+# user = User('jared', datetime.datetime.now(), 'jrjohns@mit.edu', '3280803892', '508508393', '83082982983', '930290903', '3989809808', 'hi', 'oh no', datetime.datetime.now(), '5000')
+# db.session.add(user)
+# db.session.commit()
 
 @app.route("/")
 def hello():
