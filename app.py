be190f2ff522d86071ce401debb425730bcf3bdb
jimmoffet <jimmoffet@gmail.com>
2017-11-06 09:44:19 -0500

    link in sms

--- app.py
+++ app.py
@@ -217,11 +217,7 @@ def createUser():
 		if json_dict['fbToken'] != '':
 			user.fbToken = extendToken(json_dict['fbToken'])
 
-		if json_dict['phone'] != '':
-			phone = json_dict['phone']
-			if phone[0] != 1:
-				phone = '1'+phone
-			sendSMS(phone,refname)
+		
 
 		user.positiveMessage = "COMM!Tbot says, " + name + " just showed up at the polls. Score one more for democracy!"
 		user.negativeMessage = "COMM!Tbot says, Oh No!" + name + " didn't show up at the polls today."
@@ -230,6 +226,12 @@ def createUser():
 		user.triggerDate = datetime.datetime(2018, 11, 7, 7, 00)
 		db.session.add(user)
 		db.session.commit()
+
+		if json_dict['phone'] != '':
+			phone = json_dict['phone']
+			if phone[0] != 1:
+				phone = '1'+phone
+			sendSMS(phone,refname,str(user.id))
 
 	return str(user.id)
 
