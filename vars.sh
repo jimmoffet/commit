5de147bde67b5cd04afdae9628a3a2117e3ec7f3
jimmoffet <jimmoffet@gmail.com>
2017-11-07 03:20:35 -0500

    fixes?

--- vars.sh
+++ vars.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export FLASK_APP=app.py
-export DATABASE_URL="postgresql://localhost/commit"
+export DATABASE_URL="postgresql://postgres:carleybaer@localhost:5432/commit"
 export APP_SETTINGS=config.DevelopmentConfig
